ce8c2be4af2b90671997c0a8fb6077d5c1cece77
Sanja-Duric <117166332+Sanja-Duric@users.noreply.github.com>
2022-11-17 17:57:31 +0100

    Add files via upload

---
+++ vjezba_igra_score_datum.py
@@ -0,0 +1,36 @@
+#uz score upisujemo i datum igre
+
+import random
+import json
+import datetime
+
+current_time = datetime.datetime.now()
+print(current_time)
+
+secret = random.randint(1, 30)
+attempts = 0
+
+with open("score_list.json", "r") as score_file:
+    score_list = json.loads(score_file.read())
+
+    for score_dict in score_list:
+        print(str(score_dict["attempts"]) + " attempts, date: " + score_dict.get("date"))
+
+while True:
+    guess = int(input("Pogodi tajni broj: "))
+    attempts += 1
+
+    if guess == secret:
+        score_list.append({"attempts": attempts, "date": datetime.datetime.now()})
+
+        with open("score_list.json", "w") as score_file:
+            score_file.write(json.dumps(score_list))
+
+        print("Bravo! Tajni broj je " + str(secret) + "!")
+        print("Iskorišteno pokušaja: " + str(attempts))
+        break
+    elif guess < secret:
+        print("Pokušaj s većim brojem!")
+    elif guess > secret:
+        print("Pokušaj s manjim brojem!")
+
